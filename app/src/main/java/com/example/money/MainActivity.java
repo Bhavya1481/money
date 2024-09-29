96ea445173c08cbd399f30f98b19e9046c3ffa95
Bhavya Chaudhari <bhavyachaudhari2003@gmail.com>
2024-09-30 05:17:36 +0530

    Another commit

--- app/src/main/java/com/example/money/MainActivity.java
+++ app/src/main/java/com/example/money/MainActivity.java
@@ -1,65 +1,75 @@
-//package com.example.money_management;
-//
-//import android.annotation.SuppressLint;
-//import android.content.DialogInterface;
-//import android.content.Intent;
-//import android.content.SharedPreferences;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.ImageButton;
-//import android.widget.ListView;
-//import android.widget.Toast;
-//import android.widget.SearchView;
-//import android.view.Menu;
-//import android.view.MenuInflater;
-//import android.view.MenuItem;
-//
-//
-//import androidx.appcompat.app.AlertDialog;
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private ListView listViewPersons;
-//    private Button btnAddExpense;
-//    private List<String> peopleList = new ArrayList<>();
-//    private String selectedPerson;
-//    private View btnRecords;
-//    private Button btnAddGroup;
-//    private ImageButton btnAddPerson;  // Button to add a new person
-//    private ArrayAdapter<String> adapter;
-//    private SearchView searchView;
-//    private ArrayList<String> personList;
-//
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        btnRecords = findViewById(R.id.list_btn);
-//        listViewPersons = findViewById(R.id.listview_persons);
-//        btnAddExpense = findViewById(R.id.btn_add_expense);
-//        btnAddGroup = findViewById(R.id.btn_add_group);
-//        btnAddPerson = findViewById(R.id.btn_add_person);
-//        searchView = findViewById(R.id.search_view);
-//
-//
-//        SharedPreferences sharedPreferences = getSharedPreferences("Persons", MODE_PRIVATE);
-//        String persons = sharedPreferences.getString("person_list", "");
-//
-//        if (!persons.isEmpty()) {
-//            personList = new ArrayList<>();
-//            String[] personArray = persons.split("\n");
-//            for (String person : personArray) {
-//                personList.add(person);
-//            }
-//        }
-//
+package com.example.money;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+//import androidx.navigation.Navigation;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewPersons;
+    private Button btnAddExpense;
+    private List<String> peopleList = new ArrayList<>();
+    private String selectedPerson;
+    private View btnRecords;
+    private Button btnAddGroup;
+    private ImageButton btnAddPerson;  // Button to add a new person
+    private ArrayAdapter<String> adapter;
+    private SearchView searchView;
+    private ArrayList<String> personList;
+    private EditText add_new_person;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listViewPersons = findViewById(R.id.listview_persons);
+        searchView = findViewById(R.id.search_view);
+        EditText = findViewById(R.id.add_new_person);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Persons", MODE_PRIVATE);
+        String persons = sharedPreferences.getString("person_list", "");
+
+        if (!persons.isEmpty()) {
+            personList = new ArrayList<>();
+            String[] personArray = persons.split("\n");
+            for (String person : personArray) {
+                personList.add(person);
+            }
+        }
+
+
 //        public boolean onCreateOptionsMenu(Menu menu){
 //            MenuInflater inflater = getMenuInflater();
 //            inflater.inflate(R.menu.main_menu, menu);
@@ -83,19 +93,58 @@
 //            });
 //            return true;
 //        }
+
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 //
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.btnAddexpense:
+//                        Intent addExpenseIntent = new Intent(MainActivity.this, AddExpenseActivity.class);
+//                        startActivity(addExpenseIntent);
+//                        return true;
+//                    case R.id.btnRecords:
+//                        Intent viewRecordsIntent = new Intent(MainActivity.this, ExpenseListActivity.class);
+//                        startActivity(viewRecordsIntent);
+//                        return true;
+//                    case R.id.btnAddGroup:
+//                        Toast.makeText(MainActivity.this, "Group functionality is under construction", Toast.LENGTH_SHORT).show();
+//                        return true;
 //
-//
-//
-//
-//        // Initialize adapter with the people list
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, peopleList);
-//        listViewPersons.setAdapter(adapter);
-//
-//        listViewPersons.setOnItemClickListener((parent, view, position, id) -> {
-//            selectedPerson = (String) parent.getItemAtPosition(position);
+//                }
+//                return false;
+//            }
 //        });
-//
+
+
+            bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.btnAddexpense:
+                    break;
+                case R.id.btnRecords:
+                    break;
+                case R.id.btnAddGroup:
+                    break;
+            }
+            return false;
+        });
+
+
+        private void replaceFragment(Fragment fragment)
+
+
+
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, peopleList);
+        listViewPersons.setAdapter(adapter);
+
+        listViewPersons.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPerson = (String) parent.getItemAtPosition(position);
+        });
+    }
+}
+
 //        btnAddExpense.setOnClickListener(v -> {
 //            if (selectedPerson == null) {
 //                Toast.makeText(MainActivity.this, "Please select a person first", Toast.LENGTH_SHORT).show();
@@ -105,9 +154,9 @@
 //            intent.putExtra("selected_person", selectedPerson);
 //            startActivity(intent);
 //        });
-//
-//
-//
+
+
+
 //        btnRecords.setOnClickListener(v -> {
 //            if (selectedPerson == null) {
 //                Toast.makeText(MainActivity.this, "Please select a person first", Toast.LENGTH_SHORT).show();
@@ -117,7 +166,7 @@
 //            intent.putExtra("selected_person", selectedPerson);
 //            startActivity(intent);
 //        });
-//
+
 //        btnAddGroup.setOnClickListener(v -> {
 ////            if (selectedPerson == null) {
 ////                Toast.makeText(MainActivity.this, "Please select a person first", Toast.LENGTH_SHORT).show();
@@ -127,14 +176,14 @@
 ////            intent.putExtra("selected_person", selectedPerson);
 //            startActivity(intent);
 //        });
-//
-//        // Add person functionality
+
+        // Add person functionality
 //        btnAddPerson.setOnClickListener(v -> {
 //            showAddPersonDialog();  // Show dialog to add a new person
 //        });
 //    }
-//
-//    // Method to show a dialog for adding a new person
+
+    // Method to show a dialog for adding a new person
 //    private void showAddPersonDialog() {
 //        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 //        builder.setTitle("Add Person");
@@ -329,68 +378,199 @@
 
 
 
-package com.example.money;
+//package com.example.money;
+//
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.Toast;
+//import androidx.appcompat.app.AppCompatActivity;
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    private ListView listViewPersons;
+//    private EditText etNewPerson;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Initialize views
+//        listViewPersons = findViewById(R.id.listview_persons);
+//        etNewPerson = findViewById(R.id.et_new_person);
+//
+//        // Set up Toolbar
+//        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        // Set up BottomNavigationView
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()) {
+//                case R.id.action_add_expense:
+//                    Toast.makeText(MainActivity.this, "Add Expense Clicked", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.action_records:
+//                    Toast.makeText(MainActivity.this, "Records Clicked", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.action_add_group:
+//                    Toast.makeText(MainActivity.this, "Add Group Clicked", Toast.LENGTH_SHORT).show();
+//                    return true;
+//            }
+//            return false;
+//        });
+//    }
+//
+//    // Inflate the Toolbar menu
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+//        return true;
+//    }
+//
+//    // Handle Toolbar menu item clicks
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.action_add_person) {
+//            String personName = etNewPerson.getText().toString();
+//            Toast.makeText(this, "Person Added: " + personName, Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
 
-    private ListView listViewPersons;
-    private EditText etNewPerson;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Initialize views
-        listViewPersons = findViewById(R.id.listview_persons);
-        etNewPerson = findViewById(R.id.et_new_person);
 
-        // Set up Toolbar
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        // Set up BottomNavigationView
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.action_add_expense:
-                    Toast.makeText(MainActivity.this, "Add Expense Clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_records:
-                    Toast.makeText(MainActivity.this, "Records Clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_add_group:
-                    Toast.makeText(MainActivity.this, "Add Group Clicked", Toast.LENGTH_SHORT).show();
-                    return true;
-            }
-            return false;
-        });
-    }
-
-    // Inflate the Toolbar menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    // Handle Toolbar menu item clicks
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_add_person) {
-            String personName = etNewPerson.getText().toString();
-            Toast.makeText(this, "Person Added: " + personName, Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
+//package com.example.money;
+//
+//import android.content.Context;
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.view.MenuItem;
+//import android.widget.ArrayAdapter;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.SearchView;
+//import android.widget.Toast;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    private ListView listViewPersons;
+//    private ArrayAdapter<String> personAdapter;
+//    private ArrayList<String> personList;
+//    private SharedPreferences sharedPreferences;
+//    private EditText addNewPerson;
+//    private SearchView searchView;
+//
+//    private static final String PREFS_PERSON_LIST = "PersonList";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Initialize Views
+//        listViewPersons = findViewById(R.id.listview_persons);
+//        addNewPerson = findViewById(R.id.add_new_person);
+//        searchView = findViewById(R.id.search_view);
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+//
+//        // Initialize SharedPreferences
+//        sharedPreferences = getSharedPreferences(PREFS_PERSON_LIST, Context.MODE_PRIVATE);
+//
+//        // Load saved persons
+//        personList = new ArrayList<>(getSavedPersonList());
+//
+//        // Setup ListView with Adapter
+//        personAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, personList);
+//        listViewPersons.setAdapter(personAdapter);
+//
+//        // Handle Bottom Navigation Clicks
+////        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+////            @Override
+////            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+////                switch (item.getItemId()) {
+////                    case R.id.action_add_expense:
+////                        Intent addExpenseIntent = new Intent(MainActivity.this, AddExpenseActivity.class);
+////                        startActivity(addExpenseIntent);
+////                        return true;
+////                    case R.id.action_records:
+////                        Intent viewRecordsIntent = new Intent(MainActivity.this, ExpenseListActivity.class);
+////                        startActivity(viewRecordsIntent);
+////                        return true;
+////                    case R.id.action_add_group:
+////                        Toast.makeText(MainActivity.this, "Group functionality is under construction", Toast.LENGTH_SHORT).show();
+////                        return true;
+////                }
+////                return false;
+////            }
+////        });
+//
+//        // Handle ListView Item Click
+//        listViewPersons.setOnItemClickListener((parent, view, position, id) -> {
+//            String selectedPerson = personList.get(position);
+//            Intent intent = new Intent(MainActivity.this, AddExpenseActivity.class);
+//            intent.putExtra("selected_person", selectedPerson);
+//            startActivity(intent);
+//        });
+//
+//        // Add New Person Functionality
+//        addNewPerson.setOnEditorActionListener((v, actionId, event) -> {
+//            String newPerson = addNewPerson.getText().toString().trim();
+//            if (!newPerson.isEmpty()) {
+//                personList.add(newPerson);
+//                personAdapter.notifyDataSetChanged();
+//                savePersonList(personList);
+//                addNewPerson.setText("");  // Clear input after adding
+//            } else {
+//                Toast.makeText(MainActivity.this, "Please enter a person's name", Toast.LENGTH_SHORT).show();
+//            }
+//            return true;
+//        });
+//
+//        // SearchView functionality
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                personAdapter.getFilter().filter(newText);
+//                return false;
+//            }
+//        });
+//    }
+//
+//    private Set<String> getSavedPersonList() {
+//        return sharedPreferences.getStringSet("person_list", new HashSet<>());
+//    }
+//
+//    private void savePersonList(ArrayList<String> personList) {
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        Set<String> set = new HashSet<>(personList);
+//        editor.putStringSet("person_list", set);
+//        editor.apply();
+//    }
+//}
+//
